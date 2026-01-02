6168763b05fa663dba742c8654b2e60c454a2a40
bae121 <akpablielikembright999@gmail.com>
2026-01-02 17:55:32 +0000

    personal_note_api

--- personal_note_api/notes/tests.py
+++ personal_note_api/notes/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="password123")
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, "testuser")
+
+    def test_login(self):
+        login = self.client.login(username="testuser", password="password123")
+        self.assertTrue(login)
